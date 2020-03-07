b9e655e450b4fe5b0214ffd611915b603aa24215
Anna Leoshko <annaleoshko1@gmail.com>
2020-03-07 16:43:30 +0300

    Deleted musor

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+import ephem
 
-updater = Updater('1059616795:AAGP3EVSZhQ8dUiorByqRiy9fJ-fRteyySU', use_context=True)
+from datetime import date
 
-
+updater = Updater(settings.API_KEY, use_context=True)
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -16,15 +17,14 @@ def greet_user(bot, update):
     logging.info(text)
     update.message.reply_text(text)
 
-def talk_to_me(bot, update):
-     user_text = "Приветствую тебя, {}! Твое последнее сообщение было: {}".format(update.message.chat.first_name, update.message.text)
-     logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username, update.message.chat.id, update.message.text)
-     update.message.reply_text(user_text)
+def position_recognition(bot, update):
+    today = date.today().strftime("%Y/%m/%d")
+    planet_name = update.message.text.split()
+    planet = getattr(ephem, planet_name[1])(today)
+    print(planet)
+    update.message.reply_text(f'Созвездие: {ephem.constellation(planet)[-1]}')
 
-def compliment(bot, update):
-     nice_reply = '{}, сегодня ты выглядишь просто потрясающе'.format(update.message.chat.first_name, update.message.text)
-     logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username, update.message.chat.id, update.message.text)
-     update.message.reply_text(nice_reply)
+
 
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
@@ -33,8 +33,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    dp.add_handler(MessageHandler(Filters.text, compliment))
+    dp.add_handler(CommandHandler("planet", position_recognition))
    
     mybot.start_polling()
     mybot.idle()
